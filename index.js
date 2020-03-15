b3163e131666ae9becf7beb8d85732b9349b0cf4
Filipe Mendonça <f.teotonio@it4process.de>
2020-03-15 15:38:21 -0300

    updating to deploy to heroku

--- index.js
+++ index.js
@@ -92,6 +92,8 @@ app.get('/', (req, res) => {
     })
 });
 
-app.listen(5000, function () {
-    console.log('Listening on port 5000!');
+var port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log('Listening on port ${port}!');
 });
